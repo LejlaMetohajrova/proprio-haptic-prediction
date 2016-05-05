ba67f3672de2ced493dfa2951c7a1696cc31916b
lejla <lejla.metohajrova@artinsolutions.com>
2016-05-05 19:24:12 +0200

    preventing memory error

--- mlp.py
+++ mlp.py
@@ -4,6 +4,7 @@ import pickle
 import matplotlib.pyplot as plt
 from collections import defaultdict
 from encoder import PopulationEncoder
+from scipy.sparse import lil_matrix
     
 class MultiLayerPerceptron:
 
@@ -39,16 +40,24 @@ class MultiLayerPerceptron:
         y_right = self.encoder.encode_sigmoid(right_proprio[1:].T).T
         
         # Prepare data for training
-        self.X = np.append(np.append(x1_left, x1_right, axis=1), np.append(x2_left, x2_right, axis=1), axis=1)
-        self.Y = np.append(np.append(y_left, y_right, axis=1), haptic, axis=1)
+        x1 = np.append(x1_left, x1_right, axis=1)
+        x2 = np.append(x2_left, x2_right, axis=1)
+        self.X = lil_matrix((x1.shape[0], x1.shape[1] + x2.shape[1]))
+        self.X[:, :x1.shape[1]] = x1
+        self.X[:, x1.shape[1]:] = x2
+        y = lil_matrix((y_left.shape[0], y_left.shape[1] + y_right.shape[1]))
+        y[:, :y_left.shape[1]] = y_left
+        y[:, y_left.shape[1]:] = y_right
+        self.Y = lil_matrix((y.shape[0], y.shape[1] + haptic.shape[1]))
+        self.Y[:, :y.shape[1]] = y
+        self.Y[:, y.shape[1]:] = haptic
 
-        # Randomly select and separate validation data
-        perm = np.random.permutation(len(self.X))
-        self.train_X = self.X[perm[:15000]]
-        self.train_Y = self.Y[perm[:15000]]
+        # Separate training and validation data
+        self.train_X = self.X[:75000]
+        self.train_Y = self.Y[:75000]
         
-        self.valid_X = self.X[perm[15000:]]
-        self.valid_Y = self.Y[perm[15000:]]
+        self.valid_X = self.X[75000:]
+        self.valid_Y = self.Y[75000:]
 
     def act(self, x):
         """
